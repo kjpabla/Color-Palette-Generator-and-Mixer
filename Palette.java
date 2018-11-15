095d8e97015cafe203b73002c1e8343ecf8d2f5f
kjpabla <45079983+kjpabla@users.noreply.github.com>
2018-11-15 15:49:26 -0500

    Update Palette.java

--- Palette.java
+++ Palette.java
@@ -50,58 +50,5 @@ public class Palette {
  public static Colour favourite() {
   return new DoubleColour("4500CC");
  }
-//*/
- 
- 
- 
- //YOU DON'T NEED ANYTHING BELOW THIS POINT!
- /*/
- public static Colour red() {
-  return new TwentyFour("FF0000");
- }
- 
- public static Colour green() {
-  return new TwentyFour("00FF00");
- }
- 
- public static Colour blue() {
-  return new TwentyFour("0000FF");
- }
- 
- public static Colour cyan() {
-  return new TwentyFour("00FFFF");
- }
- 
- public static Colour magenta() {
-  return new TwentyFour("FF00FF");
- }
- 
- public static Colour yellow() {
-  return new TwentyFour("FFFF00");
- }
- 
- public static Colour orange() {
-  return new TwentyFour("FFB200");
- }
- 
- public static Colour purple() {
-  return new TwentyFour("B200FF");
- }
- 
- public static Colour aqua() {
-  return new TwentyFour("37FFB1");
- }
- 
- public static Colour black() {
-  return new TwentyFour("000000");
- }
- 
- public static Colour white() {
-  return new TwentyFour("FFFFFF");
- }
- 
- public static Colour favourite() {
-  return new TwentyFour("4500CC");
- }
- //*/
+
 }
